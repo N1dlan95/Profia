5fd24898cdaa8255ff9419eac63042e5e63aa702
N1dlan95 <nidlanhendell@gmail.com>
2025-03-19 19:47:09 -0300

    Segundo comit

---
+++ springboot/src/test/java/com/Nidlan/first/FirstIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nidlan.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
